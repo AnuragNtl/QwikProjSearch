d842d7b37c91ef7e66fc061ad7c3a4c7632eae36
Anurag <anuragshrm70@gmail.com>
2019-08-18 21:45:10 +0530

    ___________

--- CommandLine/FilesystemSearcher.cpp
+++ CommandLine/FilesystemSearcher.cpp
@@ -9,11 +9,14 @@ FileSystemSearcher :: FileSystemSearcher(string filePath) : Searcher(filePath) {
 FileSystemSearcher :: FileSystemSearcher(char *data) : Searcher(data) {}
 
 vector<SearchResults> searchFor(vector<string> regexes) {
-	for_each(regexes.begin(), regexes.end(), [] (string regex) {
+	vector<SearchResults> searchResults;
+  for_each(regexes.begin(), regexes.end(), [] (string regex) {
 		boost::regex expr{regex};
 		boost::smatch matches;
 		if(boost::regex_search(data, matches, expr)) {
-			
+    for(auto it = matches.begin(); it != matches.end(); it++) {
+      searchResults.s
+    }
 		}
 	});
 }
