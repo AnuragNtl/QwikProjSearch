b87eeb54702feedd1f8fb8eb9cde116651377c4d
Anurag <anuragshrm70@gmail.com>
2019-09-22 12:33:44 +0530

    ___________

--- CommandLine/RegexTemplateSearcher.cpp
+++ CommandLine/RegexTemplateSearcher.cpp
@@ -7,7 +7,8 @@ RegexTemplate :: RegexTemplate(string regexTemplate) : regexTemplate(regexTempla
 
 string RegexTemplate :: applyAndGetRegex() {
 	for(map<string, string> :: iterator it = placeHolders.begin(); it != placeHolders.end(); it++) {
-		
+    string replaceValue = *it;
+    replaceValue = 
 	} 
 }
 
@@ -16,7 +17,7 @@ vector<SearchResults> RegexTemplateSearcher :: searchFor(vector<string> regexes)
   for_each(regexes.begin(), regexes.end(), [] (string regex) {
 		boost::regex expr{regex};
 		boost::smatch matches;
-		if(boost::regex_search(data, matches, expr)) {
+    if(boost::regex_search(data, matches, expr)) {
     for(auto it = matches.begin(); it != matches.end(); it++) {
       searchResults.insert(SearchResults(filePath, 0 , 0, *it-pData, 0));
     }
